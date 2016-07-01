23d4a12bed292bf51fd288067e01579293db8600
Maverick Chardet <chardetm@gmail.com>
2016-07-01 16:58:23 -0500

    Updated the documentation and removed the useless files in the branch

--- arapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Appliance, Script, Action
-
-admin.site.register(Appliance)
-admin.site.register(Script)
-admin.site.register(Action)
--- demo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from demo import views
-
-
-urlpatterns = [
-    url(r'^appliances/',  views.appliances),
-]
