2c5ca02f20e311f187d26bd15e48c862d316b0b5
Nick Timkovich <prometheus235@gmail.com>
2017-03-20 00:42:27 -0500

    Don't purge migrations in reset

--- reset.sh
+++ reset.sh
@@ -1,12 +1,4 @@
-#! /bin/bash
-
-APP=arapp
-
-echo "[RESET] Resetting the application..."
-rm -rf tmp
-rm -rf db.sqlite3
-rm -rf $APP/migrations
-python manage.py makemigrations $APP
+#!/bin/bash
+echo "[RESET] Resetting the application database..."
+rm -f db.sqlite3
 python manage.py migrate
-# echo "[RESET] Creating superuser 'admin' with password 'pass'..."
-# echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
